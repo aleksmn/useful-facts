fbd6aeaa4d468f6c047875d44d3aec9f36352333
aleksmn <al-mn@yandex.ru>
2024-09-26 14:31:27 +0300

    remove class for h1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
 
     return ( 
         <>
-            <h1 className="display-4">Useful Facts</h1>
+            <h1>Useful Facts</h1>
             <Link to="/facts/create" className="create-icon">
                 <BsPlusSquare />
             </Link>
